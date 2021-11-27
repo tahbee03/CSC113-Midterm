562b9b0c6fb3f601d04a96b73f36bfc70494d537
suhaimaislam <73371083+suhaimaislam@users.noreply.github.com>
2021-11-26 19:46:11 -0500

    Update ListGenerator.py

--- ListGenerator.py
+++ ListGenerator.py
@@ -10,23 +10,21 @@ GROUP MEMBERS:
 LIST GENERATOR
 '''
 
-def list_generator(RA) :
+def list_generator(RA):
+    
+    list_of_lists = []
     
     for i in range(RA[0]):
-        
         for j in range(RA[1]):
-            print(i, j, end="\n")
+            newList = []
+            newList.append(i)
+            newList.append(j)
+            list_of_lists.append(newList)
+    
+    return list_of_lists
 
-print("Test 1: RA=[3,1]")    
+# Test 1: RA = [3,1]
 RA1=[3,1] 
-list_generator(RA1);
-print("\n")
-
-print("Test 2: RA=[3,2]")
-RA2=[3,2] 
-list_generator(RA2);
-print("\n")
-
-print("Test 3: RA=[3,2]")
-RA3=[2,3] 
-list_generator(RA3);
+A1 = list_generator(RA1);
+for i in range(len(A1)):
+    print(A1[i], end=" ")
