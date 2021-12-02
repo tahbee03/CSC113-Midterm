3512103cd9512fd003a9cbb8d7769f1549366006
raynelfss <87539502+raynelfss@users.noreply.github.com>
2021-12-02 16:13:14 -0500

    Time stamps now only appear on function call.
    - Times stamps only appear on function calls.
    - Added VerifAll and VerifAll2
        * VerifAll generates all the possible arrays and verifies them.
        * VerifAll2 uses an already generated array and veifies.

--- EVERYTHING.py
+++ EVERYTHING.py
@@ -3,7 +3,6 @@ import numpy as np
 
 
 def fill_array(size, array, range_array):  # Fills the number array with user input
-    start_time = process_time_ns()
 
     for i in range(1, size + 1):
         prompt = "Element " + str(i) + ": "
@@ -14,32 +13,19 @@ def fill_array(size, array, range_array):  # Fills the number array with user in
             prompt = "Element " + str(i) + ": "
             temp = int(input(prompt))
         array = np.append(array, temp)
-
-    end_time = process_time_ns()
-    time_elapsed = end_time - start_time
-    print("[fill_array() Process Time:", time_elapsed, "ns]")  # Prints the process time of fill_array()
-
     return array
 
 
 def fill_range_array(size, array):  # Fills the range array with user input
-    start_time = process_time_ns()
 
     for i in range(1, size + 1):
         prompt = "Element " + str(i) + ": "
         temp = int(input(prompt))
         array = np.append(array, temp)
-
-    end_time = process_time_ns()
-    time_elapsed = end_time - start_time
-    print("[fill_range_array() Process Time:", time_elapsed, "ns]")  # Prints the process time of fill_range_array()
-
     return array
 
 
 def func1(num_list, range_list):  # Converts a list of numbers into its equivalent integer address; list--> address
-    start_time = process_time_ns()
-
     N = len(num_list)
     address = 0
 
@@ -48,68 +34,41 @@ def func1(num_list, range_list):  # Converts a list of numbers into its equivale
         for j in range(i + 1, N):
             temp *= range_list[j]
         address += temp
-
-    end_time = process_time_ns()
-    time_elapsed = end_time - start_time
-    print("[Function 1 Process Time:", time_elapsed, "ns]")  # Prints the process time of func1()
-
     return address
 
 
 def func2(address, range_list):  # Converts an integer address into its equivalent list of numbers;  address -> list
-    start_time = process_time_ns()
-
     N = len(range_list)
     num_list = np.array([], dtype=int)
     for i in range(0, N):
         num_list = np.append(num_list, 0)
-
     for i in range(N - 1, -1, -1):
         num_list[i] = address % range_list[i]
         address //= range_list[i]
-
-    end_time = process_time_ns()
-    time_elapsed = end_time - start_time
-    print("[Function 2 Process Time:", time_elapsed, "ns]")  # Prints the process time of func2()
-
     return num_list
 
 
 def max_addresses(range_list):  # Returns the maximum number of addresses for the given range array
-    start_time = process_time_ns()
-    
     max_a = 1
     for i in range_list:
         max_a *= i
-        
-    end_time = process_time_ns()
-    time_elapsed = end_time - start_time
-    print("[max_addresses() Process Time:", time_elapsed, "ns]")  # Prints the process time of max_addresses()
-    
     return max_a
 
 
 def list_generator(range_list):  # Returns all the lists associated with the given range array
-    start_time = process_time_ns()
-    
     array_list = np.array([], dtype=int)
     for i in range(0, max_addresses(range_list)):
         array_list = np.append(array_list, func2(i, range_list))
-        
-    end_time = process_time_ns()
-    time_elapsed = end_time - start_time
-    print("[List Generator Process Time:", time_elapsed, "ns]")  # Prints the process time of max_addresses()
-    
     return array_list.reshape(max_addresses(range_list), len(range_list))
 
 
 # verification 1 (list -> address -> list)
 def verif1(array, rangea):
-    if (np.array_equal(array,func2(func1(array, rangea), rangea))):
+    if (np.array_equal(array, func2(func1(array, rangea), rangea))):
         return True
     else:
         return False
-        
+
 # verification 2 (address -> list -> address)
 def verif2(address, rangea):
     if (address == -1):
@@ -119,48 +78,118 @@ def verif2(address, rangea):
     else:
         return False
 
-A = np.array([3,4,5], dtype=int)
-RA = np.array([4,5,6], dtype=int)
+def verifAll(rangea):
+    newArray = np.array(list_generator(rangea))
+    for i in range(0, len(newArray)):
+        if (verif1(newArray[i], rangea) == False) or (verif2(i, rangea) == False):
+            return False
+    return True
+
+def verifAll2(array, rangea):
+    for i in range(0, len(array)):
+        if (verif1(array[i], rangea) == False) or (verif2(i, rangea) == False):
+            return False
+    return True
+
+A = np.array([3, 4, 5], dtype=int)
+RA = np.array([4, 5, 6], dtype=int)
 A = np.array([], dtype=int)
 RA = np.array([], dtype=int)
 
+start_time = process_time_ns()              # Records start time of function
 size = int(input("What is the size of your array? "))
+end_time = process_time_ns()                # Records end time of function
+time_elapsed = end_time - start_time        # Time elapse is the difference between start and end times
+print("Time elapsed in operation:", time_elapsed, "ns.")
 print()
 
+
 print("Size:", size)
+start_time = process_time_ns()              # Records start time of function
 print("Enter the elements for the range array.")
 RA = fill_range_array(size, RA)
+end_time = process_time_ns()                # Records end time of function
+time_elapsed = end_time - start_time        # Time elapse is the difference between start and end times
+print("Time elapsed in operation:", time_elapsed, "ns.")
 print()
 
 print("Size:", size)
+start_time = process_time_ns()              # Records start time of function
 print("Enter the elements for the number array.")
 A = fill_array(size, A, RA)
+end_time = process_time_ns()                # Records end time of function
+time_elapsed = end_time - start_time        # Time elapse is the difference between start and end times
+print("Time elapsed in operation:", time_elapsed, "ns.")
 print()
 
 print("Array:", A)
 print("Range Array:", RA)
 
 print()
+start_time = process_time_ns()              # Records start time of function
 address = func1(A, RA)
 print("Array -> Linear Address:", A, "->", address)
+end_time = process_time_ns()                # Records end time of function
+time_elapsed = end_time - start_time        # Time elapse is the difference between start and end times
+print("Time elapsed in operation:", time_elapsed, "ns.")
 
 print()
+start_time = process_time_ns()              # Records start time of function
 A = func2(address, RA)
 print("Linear Address -> Array:", address, "->", A)
+end_time = process_time_ns()                # Records end time of function
+time_elapsed = end_time - start_time        # Time elapse is the difference between start and end times
+print("Time elapsed in operation:", time_elapsed, "ns.")
 
 print()
+start_time = process_time_ns()              # Records start time of function
 print("Maximum Number of Addresses for", RA, ":", max_addresses(RA))
+end_time = process_time_ns()                # Records end time of function
+time_elapsed = end_time - start_time        # Time elapse is the difference between start and end times
+print("Time elapsed in operation:", time_elapsed, "ns.")
 
 print()
+start_time = process_time_ns()              # Records start time of function
 all_lists = list_generator(RA)
+end_time = process_time_ns()                # Records end time of function
+time_elapsed = end_time - start_time        # Time elapse is the difference between start and end times
+print("Time elapsed in operation:", time_elapsed, "ns.")
 print("All possible number lists for", RA, ":")
+start_time = process_time_ns()              # Records start time of function
 for i in range(len(all_lists)):
     print(all_lists[i])
+end_time = process_time_ns()                # Records end time of function
+time_elapsed = end_time - start_time        # Time elapse is the difference between start and end times
+print("Time elapsed in operation:", time_elapsed, "ns.")
 
 print()
 print("Verification 1: Performs the verification between the list -> address -> list")
-print(verif1(A,RA))
+start_time = process_time_ns()              # Records start time of function
+print(verif1(A, RA))
+end_time = process_time_ns()                # Records end time of function
+time_elapsed = end_time - start_time        # Time elapse is the difference between start and end times
+print("Time elapsed in operation:", time_elapsed, "ns.")
 
 print()
 print("Verification 2: Performs the verification between the address -> list -> address")
-print(verif2(func1(A, RA),RA))
+start_time = process_time_ns()              # Records start time of function
+print(verif2(func1(A, RA), RA))
+end_time = process_time_ns()                # Records end time of function
+time_elapsed = end_time - start_time        # Time elapse is the difference between start and end times
+print("Time elapsed in operation:", time_elapsed, "ns.")
+
+print()
+print("Verification ( Generating all possible number lists for", RA, ") by running Verifications 1 and 2:")
+start_time = process_time_ns()              # Records start time of function
+print(verifAll(RA))
+end_time = process_time_ns()                # Records end time of function
+time_elapsed = end_time - start_time        # Time elapse is the difference between start and end times
+print("Time elapsed in operation:", time_elapsed, "ns.")
+
+print()
+print("Verification ( Using generated all possible number lists for", RA, ") by running Verifications 1 and 2:")
+start_time = process_time_ns()              # Records start time of function
+print(verifAll2(all_lists, RA))
+end_time = process_time_ns()                # Records end time of function
+time_elapsed = end_time - start_time        # Time elapse is the difference between start and end times
+print("Time elapsed in operation:", time_elapsed, "ns.")
